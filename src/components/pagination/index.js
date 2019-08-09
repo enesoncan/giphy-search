266c31cc072b25779f54e0131a0ec1de68f3feb7
Enes Öncan <enesoncn@gmail.com>
2019-08-09 13:49:45 +0300

    fix pagination

--- src/components/pagination/index.js
+++ src/components/pagination/index.js
@@ -9,31 +9,45 @@ class Pagination extends Component {
 
   render() {
     const { gifsData } = this.props;
-    const paginationData = gifsData.pagination;
-    let ulList = [];
+    const ulList = [];
 
-    if (typeof paginationData !== "undefined") {
-      var count = paginationData.count;
-      var total_count = paginationData.total_count;
-      var pageNumber = Math.floor(total_count / count);
-    }
-    for (let i = 0; i < pageNumber; i++) {
-      if (i === 5) {
-        break;
+    if (typeof gifsData.pagination !== "undefined") {
+      const paginationData = gifsData.pagination;
+
+      const count = paginationData.count;
+      const total_count = paginationData.total_count;
+      const pageNumber = Math.floor(total_count / count);
+
+      for (let i = 0; i < pageNumber; i++) {
+        if (i === 5) {
+          break;
+        }
+        ulList.push(i);
       }
-      ulList.push(
-        <li
-          key={i}
-          value={i}
-          className="pagination-item"
-          onClick={e => this.onPaginate(e)}
-        >
-          {i + 1}
-        </li>
-      );
     }
 
-    return <ul className="pagination">{ulList}</ul>;
+    return (
+      <div>
+        {ulList.length > 0 ? (
+          <ul className="pagination">
+            {ulList.map(item => {
+              return (
+                <li
+                  key={item}
+                  value={item}
+                  className="pagination-item"
+                  onClick={e => this.onPaginate(e)}
+                >
+                  {item + 1}
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          ""
+        )}
+      </div>
+    );
   }
 }
 
