11152443da2698ab91fe07775e79ec32ee7b7564
Enes Öncan <enesoncn@gmail.com>
2019-08-27 14:38:51 +0300

    fix navigation and added active tab feature

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-function Nav() {
-  return (
-    <ul className="navigate">
-      <Link to="/">
-        <li>Home</li>
-      </Link>
-      <Link to="contact">
-        <li>Contact</li>
-      </Link>
-    </ul>
-  );
+class Nav extends Component {
+  state = {
+    active: false
+  };
+
+  activeHandler = () => {
+    this.setState({
+      active: !this.state.active
+    });
+  };
+
+  render() {
+    return (
+      <ul className="navigate">
+        <li>
+          <NavLink exact activeClassName="active" to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact activeClassName="active" to="/contact">
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+    );
+  }
 }
 
 export default Nav;
